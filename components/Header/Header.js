16d9cead8fc2a63d84f3b623ad9eec7598cd8eca
Gustav Pursche <pursche@posteo.de>
2021-01-21 21:49:39 +0100

    Header: make menu adaptive to the window width

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -4,10 +4,13 @@ import Link from 'next/link';
 
 import { useStore } from '@/lib/store';
 
+import BarsIcon from '@/public/icons/bars.svg';
 import BookmarkIcon from '@/public/icons/bookmark-regular.svg';
 import Logo from '@/components/Logo';
 import MenuItem from '@/components/MenuItem';
 import SearchIcon from '@/public/icons/search-regular.svg';
+
+import * as styles from './header.module.css';
 
 export default function Header() {
   const { locale, locales } = useRouter();
@@ -23,17 +26,17 @@ export default function Header() {
   const cta = items && items.slice(items.length - 1);
 
   return (
-    <header className="bg-orange-800 p-5 text-white flex flex-row justify-center w-full overflow-x-hidden">
+    <header className="bg-orange-800 px-5 pt-5 pb-6 text-white flex flex-row justify-center w-full overflow-x-hidden">
       <div className="flex flex-row align-bottom max-w-wide w-full">
         <Link href="/">
-          <a className="flex items-end p-2 mr-5">
-            <Logo />
+          <a className="flex items-end justify-center p-2">
+            <Logo className="w-auto h-10 sm:h-12 md:h-14" />
           </a>
         </Link>
 
-        <nav className="flex flex-col space-y-5">
+        <nav className="flex flex-col w-full">
           {headerSecondaryItems && headerSecondaryItems?.length > 0 && (
-            <div className="md:flex-row md:space-x-3 md:justify-self-end md:ml-auto pr-48 hidden md:flex">
+            <div className="md:flex-row md:space-x-3 md:justify-self-end md:ml-auto pr-52 hidden md:flex md:mb-3">
               {otherLocales.map((currentLocale, index) => (
                 <MenuItem
                   path="/"
@@ -71,19 +74,29 @@ export default function Header() {
           )}
 
           {primaryItems && primaryItems.length > 0 && (
-            <div className="flex flex-row space-x-10 items-end ml-10">
-              {primaryItems.map((item) => (
+            <div className="flex flex-row justify-around w-full pl-5 sm:pl-10 md:pl-20 items-end mt-auto self-end">
+              {primaryItems.map((item, index) => (
                 <MenuItem
                   key={`menu-${item.label}`}
                   {...item}
-                  className="font-rubik text-base uppercase font-bold leading-none hover:bg-white hover:text-orange-800 p-2 whitespace-nowrap"
+                  className={`font-rubik text-small md:text-base uppercase font-bold leading-none hover:bg-white hover:text-orange-800 p-2 whitespace-nowrap ${
+                    styles.item
+                  } ${styles[`item--${index + 1}`]}`}
                 />
               ))}
 
               <MenuItem
                 {...cta[0]}
-                className="font-rubik text-2xs uppercase leading-none text-gray-700 hover:text-white hover:bg-black px-7 py-4 bg-white rounded-3xl whitespace-nowrap"
+                className="font-rubik text-2xs uppercase leading-none text-gray-700 hover:text-white hover:bg-black px-6 md:px-7 py-4 bg-white rounded-3xl whitespace-nowrap"
               />
+
+              <button
+                type="button"
+                className={`font-rubik text-3xs uppercase text-white text-center p-0 leading-none ${styles.burger}`}
+              >
+                <BarsIcon className="w-12 h-12" />
+                Menu
+              </button>
             </div>
           )}
         </nav>
