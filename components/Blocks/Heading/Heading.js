0c206c95e789b64a76765e61470ffa2f85cd51ea
Gustav Pursche <pursche@posteo.de>
2021-01-22 13:40:03 +0100

    HeadingBlock: fix x position on small screens

--- components/Blocks/Heading/Heading.js
+++ components/Blocks/Heading/Heading.js
@@ -5,7 +5,7 @@ export default function HeadingBlock({ level, text, kicker }) {
     <Heading
       level={parseInt(level.substr(1), 10)}
       kicker={kicker}
-      className="pt-20 col-span-full md:col-start-3 md:col-span-9"
+      className="pt-20 px-10 md:px-0 col-span-full md:col-start-3 md:col-span-9"
     >
       {text}
     </Heading>
