15ae37e3fc8ef0d8f03f27e16e10b17d295f6c96
Gustav Pursche <pursche@posteo.de>
2021-01-28 21:36:33 +0100

    SectionNavigation: add drop shadow

--- components/SectionNavigation/SectionNavigation.js
+++ components/SectionNavigation/SectionNavigation.js
@@ -2,13 +2,19 @@ import { useRouter } from 'next/router';
 
 import MenuItem from '@/components/MenuItem';
 
+import * as styles from './sectionNavigation.module.css';
+
 export default function SectionNavigation({ items, className }) {
   const { asPath } = useRouter();
 
   return (
     <div
-      className={`border-t border-gray-400 bg-orange-200 grid grid-layout-primary col-span-full sticky top-0 z-30 ${className}`}
+      className={`bg-orange-200 grid grid-layout-primary col-span-full sticky top-0 z-30 ${styles.navigation} ${className}`}
     >
+      <span
+        className={`w-full bg-gray-400 absolute top-0 left-0 ${styles.border}`}
+      />
+
       <nav className="py-8 px-8 md:px-0 col-span-full md:col-start-3 md:col-span-10 flex flex-row space-x-5 flex-nowrap overflow-x-auto w-full">
         {items.map((item, index) => {
           const isActive = asPath === item.path;
