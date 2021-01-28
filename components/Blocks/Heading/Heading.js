f9bd6b68bff058052b5253664d0b9b6c1f91b672
Gustav Pursche <pursche@posteo.de>
2021-01-28 21:53:48 +0100

    HeadingBlock: decrease spacing to above block

--- components/Blocks/Heading/Heading.js
+++ components/Blocks/Heading/Heading.js
@@ -9,7 +9,7 @@ export default function HeadingBlock({ level, text, kicker, children }) {
     <Heading
       level={normalizedLevel}
       kicker={kicker}
-      className="pt-20 px-10 md:px-0 col-span-full md:col-start-3 md:col-span-9"
+      className="pt-14 px-10 md:px-0 col-span-full md:col-start-3 md:col-span-9"
     >
       {normalizedChildren}
     </Heading>
