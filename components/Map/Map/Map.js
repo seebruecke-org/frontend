c593c3c6d979eb22d179d8d581074ec3966e6945
Gustav Pursche <pursche@posteo.de>
2021-02-10 12:01:53 +0100

    TakePart: adapt layout for small screens

--- components/Map/Map/Map.js
+++ components/Map/Map/Map.js
@@ -1,5 +1,7 @@
 export default function Map() {
   return (
-    <div className="bg-gray-400 col-start-1 col-span-6 h-screen">Karte</div>
+    <div className="bg-gray-400 col-span-full md:col-start-1 md:col-span-6 h-96 md:h-screen">
+      Karte
+    </div>
   );
 }
