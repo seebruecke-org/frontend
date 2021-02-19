ecc4e97e4f9f3e99ed0fc9f28a684b28b6b354d0
Gustav Pursche <pursche@posteo.de>
2021-02-19 21:46:42 +0100

    CTA: add drop shadow

--- components/CTA/CTA.js
+++ components/CTA/CTA.js
@@ -1,9 +1,13 @@
 import Link from 'next/link';
 
+import * as styles from './cta.module.css';
+
 export default function CTA({ path, label }) {
   return (
     <Link href={path}>
-      <a className="font-rubik font-bold text-small md:text-base uppercase bg-white text-black py-4 md:py-6 px-8 md:px-10 rounded-3xl hover:bg-black hover:text-white">
+      <a
+        className={`font-rubik font-bold text-small md:text-base uppercase bg-white text-black py-4 md:py-6 px-8 md:px-10 rounded-3xl hover:bg-black hover:text-white ${styles.cta}`}
+      >
         {label}
       </a>
     </Link>
