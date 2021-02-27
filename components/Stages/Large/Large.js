3d38cc795ed3d0d8a8b8f331a2ad0669c01e942e
Gustav Pursche <pursche@posteo.de>
2021-02-27 21:37:44 +0100

    StageLarge: improve vertical padding on small screens

--- components/Stages/Large/Large.js
+++ components/Stages/Large/Large.js
@@ -14,7 +14,7 @@ export default function StageLarge({ image, className, title, cta = null }) {
         className="col-span-full h-full w-full relative md:absolute"
       />
 
-      <div className="bg-orange-200 py-12 md:py-28 px-8 md:px-20 md:w-8/12 absolute -bottom-48 md:-bottom-32 left-8 md:left-auto right-8 md:right-0 w-auto">
+      <div className="bg-orange-200 py-20 md:py-28 px-8 md:px-20 md:w-8/12 absolute -bottom-48 md:-bottom-32 left-8 md:left-auto right-8 md:right-0 w-auto">
         <div className="max-w-6xl">
           <Heading kicker="Kampagne" level={1}>
             {title}
