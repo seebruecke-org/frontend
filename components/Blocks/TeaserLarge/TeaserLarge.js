4c5a32863527f1025217636bef9b8df29d9978a6
Gustav Pursche <pursche@posteo.de>
2021-02-27 12:02:24 +0100

    TeaserLarge: move into Teasers/ and create wrapping block component

--- components/Blocks/TeaserLarge/TeaserLarge.js
+++ components/Blocks/TeaserLarge/TeaserLarge.js
@@ -1,52 +1,5 @@
-import CTA from '@/components/CTA';
+import TeaserLarge from '@/components/Teaser/TeaserLarge';
 
-export default function TeaserLarge({ title, intro, cta, tType = 'internal' }) {
-  let background = '';
-  let ctaInverse = true;
-
-  switch (tType) {
-    case 'campaign':
-      background = 'bg-orange-200';
-      break;
-
-    case 'action':
-      background = 'bg-turquoise-300';
-      break;
-
-    case 'general':
-      background = 'bg-white';
-      break;
-
-    default:
-      background = 'bg-orange-800 text-white';
-      ctaInverse = false;
-  }
-
-  return (
-    <section
-      className={`py-12 ${
-        tType === 'general' ? 'md:py-0' : 'md:py-16'
-      } col-span-full md:col-start-3 md:col-span-9`}
-    >
-      <div
-        className={`${background} pl-10 md:pl-0 py-16 md:py-24 pr-24 relative`}
-      >
-        <h2 className="font-brezel text-xl md:text-2xl font-bold italic leading-tight">
-          {title}
-        </h2>
-
-        <p className="font-rubik text-small md:text-medium leading-tight mt-4">
-          {intro}
-        </p>
-
-        <div className="mt-8 md:mt-16">
-          <CTA {...cta} inverse={ctaInverse} />
-        </div>
-
-        <span
-          className={`absolute right-full h-full top-0 w-40 hidden md:block ${background}`}
-        />
-      </div>
-    </section>
-  );
+export default function TeaserLargeBlock({ tType, ...props }) {
+  return <TeaserLarge {...props} type={tType} />;
 }
---
+++ components/Teaser/TeaserLarge/TeaserLarge.js
@@ -0,0 +1,52 @@
+import CTA from '@/components/CTA';
+
+export default function TeaserLarge({ title, intro, cta, type = 'internal' }) {
+  let background = '';
+  let ctaInverse = true;
+
+  switch (type) {
+    case 'campaign':
+      background = 'bg-orange-200';
+      break;
+
+    case 'action':
+      background = 'bg-turquoise-300';
+      break;
+
+    case 'general':
+      background = 'bg-white';
+      break;
+
+    default:
+      background = 'bg-orange-800 text-white';
+      ctaInverse = false;
+  }
+
+  return (
+    <section
+      className={`py-12 ${
+        type === 'general' ? 'md:py-0' : 'md:py-16'
+      } col-span-full md:col-start-3 md:col-span-9`}
+    >
+      <div
+        className={`${background} pl-10 md:pl-0 py-16 md:py-24 pr-24 relative`}
+      >
+        <h2 className="font-brezel text-xl md:text-2xl font-bold italic leading-tight">
+          {title}
+        </h2>
+
+        <p className="font-rubik text-small md:text-medium leading-tight mt-4">
+          {intro}
+        </p>
+
+        <div className="mt-8 md:mt-16">
+          <CTA {...cta} inverse={ctaInverse} />
+        </div>
+
+        <span
+          className={`absolute right-full h-full top-0 w-40 hidden md:block ${background}`}
+        />
+      </div>
+    </section>
+  );
+}
