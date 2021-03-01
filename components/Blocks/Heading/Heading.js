a8d0369681ce4b2aee26aaa8d21747c11c11c858
Gustav Pursche <pursche@posteo.de>
2021-03-01 20:42:20 +0100

    HeadingBlock: improve top margin calculation

--- components/Blocks/Heading/Heading.js
+++ components/Blocks/Heading/Heading.js
@@ -1,15 +1,36 @@
 import Heading from '@/components/Heading';
 
-export default function HeadingBlock({ level, text, kicker, children, as }) {
+import { blockNameMatches } from '@/lib/blocks';
+
+export default function HeadingBlock({
+  level,
+  text,
+  kicker,
+  children,
+  as,
+  blockContext = {}
+}) {
+  const { previous } = blockContext;
   const normalizedLevel =
     typeof level === 'string' ? parseInt(level.substr(1), 10) : level;
   const normalizedChildren = text || children;
+  let marginTop = 'pt-10 md:pt-14';
+
+  if (normalizedLevel === 1) {
+    marginTop = 'pt-14 md:pt-24';
+
+    if (blockNameMatches(previous, 'SubNavigation')) {
+      marginTop = 'pt-14 md:pt-32';
+    }
+  } else if (blockNameMatches(previous, 'SubNavigation')) {
+    marginTop = 'pt-14 md:pt-24';
+  }
 
   return (
     <Heading
       level={normalizedLevel}
       kicker={kicker}
-      className="pt-14 px-8 md:px-0 col-span-full md:col-start-3 md:col-span-9"
+      className={`${marginTop} px-8 md:px-0 col-span-full md:col-start-3 md:col-span-9`}
       as={as}
     >
       {normalizedChildren}
