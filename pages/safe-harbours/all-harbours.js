e0cb07a4d3aec53381db22d5e1293633cf8dc237
Gustav Pursche <pursche@posteo.de>
2021-03-01 20:55:51 +0100

    SafeHarbours: implement city filter

--- pages/safe-harbours/all-harbours.js
+++ pages/safe-harbours/all-harbours.js
@@ -2,21 +2,65 @@ import { getPage } from '@/lib/pages';
 import { fetchAllGroups } from '@/lib/take-part';
 import { query as queryGlobalData } from '@/lib/global';
 import { useI18n } from 'next-localization';
+import { memo, useState, useEffect } from 'react';
 
 import { FederalCountry, Country, Map } from '@/components/Map';
 import SafeHarbour from '@/components/Teaser/SafeHarbour';
 import BlockSwitch from '@/components/BlockSwitch';
 import Form, { Row, TextInput } from '@/components/Form';
 
-export default function SafeHarboursOverview({ cities, page }) {
+function filterCities(cities, term) {
+  return Object.keys(cities).reduce((acc, countryName) => {
+    const federalCountries = cities[countryName].countries;
+    const matchingFederalCountries = {};
+
+    Object.keys(federalCountries).forEach((federalCountryName) => {
+      const federalCountry = federalCountries[federalCountryName];
+
+      federalCountry.cities.forEach((city) => {
+        const { name: cityName } = city;
+
+        if (cityName.includes(term)) {
+          if (!matchingFederalCountries[federalCountryName]) {
+            matchingFederalCountries[federalCountryName] = { cities: [] };
+          }
+
+          matchingFederalCountries[federalCountryName].cities.push(city);
+        }
+      });
+    });
+
+    if (Object.keys(matchingFederalCountries).length > 0) {
+      acc[countryName] = {
+        countries: matchingFederalCountries
+      };
+    }
+
+    return acc;
+  }, {});
+}
+
+const MemoizedMap = memo(Map);
+
+export default function SafeHarboursOverview({ cities: defaultCities, page }) {
   const i18n = useI18n();
+  const [filterValue, setFilterValue] = useState(null);
+  const [cities, setCities] = useState(defaultCities);
+
+  useEffect(() => {
+    if (filterValue) {
+      setCities(filterCities(defaultCities, filterValue));
+    } else {
+      setCities(defaultCities);
+    }
+  }, [filterValue]);
 
   return (
     <article>
       <BlockSwitch blocks={page?.content} />
 
       <div className="grid grid-layout-primary">
-        <Map />
+        <MemoizedMap />
 
         <div className="col-span-full md:col-start-7 md:col-span-8 pb-10 md:pb-36">
           <Form primaryGrid={false} className="grid-cols-6">
@@ -24,6 +68,10 @@ export default function SafeHarboursOverview({ cities, page }) {
               <TextInput
                 name="filter"
                 placeholder={i18n.t('group.searchCity')}
+                onChange={(event) => {
+                  setFilterValue(event.target.value);
+                }}
+                autoComplete="off"
               />
             </Row>
           </Form>
