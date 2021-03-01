290f71baffc84b537827b73873daebd3760bd2e6
Gustav Pursche <pursche@posteo.de>
2021-03-01 21:04:03 +0100

    Richtext: increase margin-top when previous element is the subnav

--- components/Blocks/Richtext/Richtext.js
+++ components/Blocks/Richtext/Richtext.js
@@ -5,6 +5,8 @@ import Link from './Link';
 import List, { ListItem } from './List';
 import Paragraph from './Paragraph';
 
+import { blockNameMatches } from '@/lib/blocks';
+
 const RENDERERS = {
   heading: ({ level, children }) => {
     return <Heading level={level}>{children}</Heading>;
@@ -31,11 +33,18 @@ const RENDERERS = {
   }
 };
 
-export default function Richtext({ richtext }) {
+export default function Richtext({ richtext, blockContext = {} }) {
+  const { previous } = blockContext;
+  let marginTop = 'mt-5 md:mt-10';
+
+  if (blockNameMatches(previous, 'SubNavigation')) {
+    marginTop = 'mt-12 md:mt-20';
+  }
+
   return (
     <Markdown
       renderers={RENDERERS}
-      className="col-span-full md:col-start-3 md:col-span-9 flex flex-col space-y-10 my-5 md:my-10"
+      className={`col-span-full md:col-start-3 md:col-span-9 flex flex-col space-y-10 ${marginTop} mb-5 md:mb-10`}
     >
       {richtext}
     </Markdown>
