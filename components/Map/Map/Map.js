894eb62b49aa4f2bd6baeb8507c807d4ddec9a91
Gustav Pursche <pursche@posteo.de>
2021-03-03 21:02:28 +0100

    Map: fix build by checking if markers exist

--- components/Map/Map/Map.js
+++ components/Map/Map/Map.js
@@ -8,24 +8,27 @@ export default function Map({ marker }) {
     <div className="col-span-full md:col-start-1 md:col-span-6 relative">
       <div className="bg-gray-400 h-96 md:h-screen sticky top-0 overflow-hidden">
         <MapboxMap>
-          <Layer
-            id="points"
-            type="circle"
-            paint={{ 'circle-color': 'red', 'circle-radius': 5 }}
-          >
-            {marker.map(({ title, coordinates }, index) => {
-              const mbCoordinates = coordinates
-                .split(',')
-                .reverse()
-                .map((part) => parseFloat(part.trim(), 10));
+          {marker && (
+            <Layer
+              id="points"
+              type="circle"
+              paint={{ 'circle-color': 'red', 'circle-radius': 5 }}
+            >
+              {marker.map(({ title, coordinates }, index) => {
+                const mbCoordinates = coordinates
+                  .split(',')
+                  .reverse()
+                  .map((part) => parseFloat(part.trim(), 10));
 
-              console.log(mbCoordinates);
-
-              return (
-                <Feature coordinates={mbCoordinates} key={`marker-${index}`} />
-              );
-            })}
-          </Layer>
+                return (
+                  <Feature
+                    coordinates={mbCoordinates}
+                    key={`marker-${index}`}
+                  />
+                );
+              })}
+            </Layer>
+          )}
         </MapboxMap>
       </div>
     </div>
