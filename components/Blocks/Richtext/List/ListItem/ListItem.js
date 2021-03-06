a27f934f67a647043624a6f4360394e29dd086ed
Gustav Pursche <pursche@posteo.de>
2021-03-06 11:04:26 +0100

    List: improve ::marker styles

--- components/Blocks/Richtext/List/ListItem/ListItem.js
+++ components/Blocks/Richtext/List/ListItem/ListItem.js
@@ -1,3 +1,5 @@
+import * as styles from './listItem.module.css';
+
 export default function ListItem({ children }) {
-  return <li>{children}</li>;
+  return <li className={styles.listItem}>{children}</li>;
 }
