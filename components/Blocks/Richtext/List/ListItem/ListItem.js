71c6c543c28f43c50e5617b9b25a59ffdc3c2f4e
Gustav Pursche <pursche@posteo.de>
2021-03-06 11:07:00 +0100

    ListItem: improve disc rendering

--- components/Blocks/Richtext/List/ListItem/ListItem.js
+++ components/Blocks/Richtext/List/ListItem/ListItem.js
@@ -1,5 +1,5 @@
 import * as styles from './listItem.module.css';
 
 export default function ListItem({ children }) {
-  return <li className={styles.listItem}>{children}</li>;
+  return <li className={`${styles.listItem} font-rubik`}>{children}</li>;
 }
