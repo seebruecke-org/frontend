611d08feb360c82cc91fde7e3366a8360f4d5ed3
Gustav Pursche <pursche@posteo.de>
2021-03-10 09:03:21 +0100

    Improve styles

--- components/Gallery/Gallery.js
+++ components/Gallery/Gallery.js
@@ -3,10 +3,17 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { useState } from 'react';
 import Measure from 'react-measure';
 
+import ChevronLeftIcon from '@/public/icons/chevron-left-regular.svg';
+import ChevronRightIcon from '@/public/icons/chevron-right-regular.svg';
+
+import * as styles from './gallery.module.css';
+
 SwiperCore.use([Navigation, Keyboard, Mousewheel, A11y]);
 
+const SLIDE_GAP = 20;
+
 export default function Gallery({ items }) {
-  const [offsetLet, setOffsetLeft] = useState({});
+  const [offsetLeft, setOffsetLeft] = useState({});
   const [swiperInstance, setSwiperInstance] = useState(null);
   const [currentSlideIndex, setCurrentSlideIndex] = useState(0);
 
@@ -26,62 +33,61 @@ export default function Gallery({ items }) {
         )}
       </Measure>
 
-      {offsetLet && (
-        <div className="col-span-full overflow-hidden relative">
-          {currentSlideIndex > 0 && (
-            <button
-              type="button"
-              style={{
-                backgroundImage:
-                  'linear-gradient(to left, rgba(255, 255, 255, 0), white)'
-              }}
-              className="h-full w-20 absolute top-0 left-0 z-10 hidden md:block"
-              onClick={() => {
-                if (swiperInstance) {
-                  swiperInstance.slidePrev();
-                }
-              }}
-            >
-              Previous
-            </button>
-          )}
-
-          <Swiper
-            freeMode
-            slidesPerView="auto"
-            spaceBetween={30}
-            mousewheel={true}
-            onSlideChange={({ activeIndex }) => {
-              setCurrentSlideIndex(activeIndex);
-            }}
-            onSwiper={(swiper) => setSwiperInstance(swiper)}
-          >
-            <SwiperSlide style={{ width: `${offsetLet - 30}px` }} />
-
-            {items.map(({ media }) => (
-              <SwiperSlide style={{ width: '35%' }}>
-                <img src={media} />
-              </SwiperSlide>
-            ))}
-          </Swiper>
-
+      <div className="col-span-full overflow-hidden relative">
+        {currentSlideIndex > 1 && (
           <button
             type="button"
-            style={{
-              backgroundImage:
-                'linear-gradient(to right, rgba(255, 255, 255, 0), white)'
-            }}
-            className="h-full w-20 absolute top-0 right-0 z-10 hidden md:block"
+            className={`h-full w-20 absolute top-0 left-0 z-10 hidden md:flex justify-center pr-16 pl-8 ${styles.controlPrev}`}
             onClick={() => {
               if (swiperInstance) {
-                swiperInstance.slideNext();
+                swiperInstance.slidePrev();
               }
             }}
+            aria-label="Previous Slide"
           >
-            Next
+            <ChevronLeftIcon className="w-6 h-auto flex-shrink-0" />
           </button>
-        </div>
-      )}
+        )}
+
+        <Swiper
+          freeMode
+          slidesPerView="auto"
+          spaceBetween={SLIDE_GAP}
+          mousewheel={{
+            forceToAxis: true
+          }}
+          onSlideChange={({ activeIndex }) => {
+            setCurrentSlideIndex(activeIndex);
+          }}
+          onSwiper={(swiper) => setSwiperInstance(swiper)}
+        >
+          {offsetLeft > 0 && (
+            <SwiperSlide style={{ width: `${offsetLeft - SLIDE_GAP}px` }} />
+          )}
+
+          {items.map(({ media }) => (
+            <SwiperSlide className={styles.slide}>
+              <img
+                src={media}
+                className="w-auto h-full max-w-full"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+
+        <button
+          type="button"
+          className={`h-full pl-16 pr-8 absolute top-0 right-0 z-10 hidden md:flex justify-center ${styles.controlNext}`}
+          onClick={() => {
+            if (swiperInstance) {
+              swiperInstance.slideNext();
+            }
+          }}
+          aria-label="Next Slide"
+        >
+          <ChevronRightIcon className="w-6 h-auto flex-shrink-0" />
+        </button>
+      </div>
     </>
   );
 }
