507a8d067234e1f18be2d96e10c85b7d8690e215
Gustav Pursche <pursche@posteo.de>
2021-03-19 21:41:12 +0100

    StageLarge: fix subnavigation conditional

--- components/Stages/Large/Large.js
+++ components/Stages/Large/Large.js
@@ -12,6 +12,9 @@ export default function StageLarge({
   cta = null,
   subnavigation
 }) {
+  const hasSubnavigation =
+    subnavigation && Array.isArray(subnavigation) && subnavigation.length > 0;
+
   return (
     <div className={`relative mb-40 md:mb-52 ${styles.stage} ${className}`}>
       <Image
@@ -24,7 +27,7 @@ export default function StageLarge({
 
       <div
         className={`bg-orange-200 ${
-          subnavigation ? 'pt-20 md:pt-28' : 'py-20 md:py-28'
+          hasSubnavigation ? 'pt-20 md:pt-28' : 'py-20 md:py-28'
         } md:w-8/12 absolute -bottom-48 md:-bottom-32 left-8 md:left-auto right-8 md:right-0 w-auto`}
       >
         <div className="max-w-6xl px-8 md:px-20">
@@ -39,7 +42,7 @@ export default function StageLarge({
           )}
         </div>
 
-        {subnavigation && (
+        {hasSubnavigation && (
           <SectionNavigation
             items={subnavigation}
             primaryGrid={false}
