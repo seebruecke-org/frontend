82f02859e8690b978477f0149bf6b85cce6189c2
Gustav Pursche <pursche@posteo.de>
2021-04-01 19:46:22 +0200

    Media: hide caption in most cases behind an info icon

--- components/Blocks/Media/Media.js
+++ components/Blocks/Media/Media.js
@@ -7,6 +7,7 @@ export default function MediaBlock({ image }) {
       className="col-span-full md:col-start-2 md:col-span-10 my-20 grid grid-cols-10 md:place-items-end"
       classNameImage="col-span-full md:col-start-1 md:col-span-8"
       classNameCaption="col-span-full md:col-start-9 md:col-span-2 md:pb-0"
+      showCaption={true}
     />
   );
 }
--- components/Media/Media.js
+++ components/Media/Media.js
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import NextImage from 'next/image';
 
+import InfoCircleIcon from '@/public/icons/info-circle.svg';
 import Richtext from '@/components/Blocks/Richtext';
 
 export default function Media({
@@ -9,6 +11,7 @@ export default function Media({
   className,
   classNameImage,
   classNameCaption,
+  showCaption = false,
   ...props
 }) {
   if (!image || !image.media) {
@@ -26,13 +29,37 @@ export default function Media({
     ...props
   };
 
+  const showCaptionAsOverlay = !showCaption;
+  const [isCaptionOpen, setIsCaptionOpen] = useState(showCaption);
+  const hasCaption = caption || imageCaption;
+
   return (
-    <figure className={`leading-none text-none ${className}`}>
+    <figure className={`leading-none text-none relative ${className}`}>
       <div className={classNameImage}>{<NextImage {...imageProps} />}</div>
 
-      {(caption || imageCaption) && (
+      {!showCaption && hasCaption && (
+        <button
+          type="button"
+          onClick={() => {
+            setIsCaptionOpen(!isCaptionOpen);
+          }}
+          className="absolute bottom-4 left-4"
+        >
+          <InfoCircleIcon
+            className={`w-8 h-auto hover:text-white ${
+              isCaptionOpen && 'text-white'
+            }`}
+          />
+        </button>
+      )}
+
+      {isCaptionOpen && hasCaption && (
         <figcaption
-          className={`font-rubik italic text-2xs px-10 py-5 md:p-5 text-gray-600 font-rubik-features ${classNameCaption}`}
+          className={`font-rubik italic text-2xs px-10 py-5 md:p-5 text-gray-600 font-rubik-features ${classNameCaption} ${
+            showCaptionAsOverlay &&
+            isCaptionOpen &&
+            'absolute -bottom-8 left-16 bg-white z-20'
+          }`}
         >
           <Richtext richtext={caption || imageCaption} isSmall />
         </figcaption>
