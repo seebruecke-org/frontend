05ac21de17340bef5893a62810ea093a99b96262
Gustav Pursche <pursche@posteo.de>
2021-04-02 22:00:19 +0200

    Breadcrumbs: fix links, hide all items except the last on mobile

--- components/Breadcrumbs/Breadcrumbs.js
+++ components/Breadcrumbs/Breadcrumbs.js
@@ -6,22 +6,30 @@ export default function Breadcrumbs({ crumbs }) {
   }
 
   return (
-    <ul className="col-start-3 col-span-10 flex py-5">
-      {crumbs.map((crumb, index) => (
-        <li key={`breadcrumb-${index}`}>
-          {index !== 0 && (
-            <span className="font-rubik text-gray-400 mx-2 uppercase text-2xs md:text-xs">
-              &gt;
-            </span>
-          )}
-          <StrapiLink
-            {...crumb}
-            className={`font-rubik text-gray-600 uppercase text-2xs md:text-xs ${
-              crumb.path && 'hover:underline'
-            }`}
-          />
-        </li>
-      ))}
+    <ul className="col-start-3 col-span-10 flex py-5 px-8">
+      {crumbs.map((crumb, index) => {
+        const isFirst = index === 0;
+        const isLast = index + 1 === crumbs.length;
+
+        return (
+          <li
+            key={`breadcrumb-${index}`}
+            className={`${isLast ? '' : 'hidden md:inline-flex'}`}
+          >
+            {!isFirst && (
+              <span className="font-rubik text-gray-400 mx-2 uppercase text-2xs md:text-xs hidden md:inline-flex">
+                &gt;
+              </span>
+            )}
+            <StrapiLink
+              link={crumb}
+              className={`font-rubik text-gray-600 uppercase text-2xs md:text-xs ${
+                crumb.url && 'hover:underline'
+              }`}
+            />
+          </li>
+        );
+      })}
     </ul>
   );
 }
