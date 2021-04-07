526ccaf9a5c998ee719ab5f40266368c990bf6f4
Gustav Pursche <pursche@posteo.de>
2021-04-07 22:18:46 +0200

    Next: enable webpack 5

--- next.config.js
+++ next.config.js
@@ -95,5 +95,9 @@ module.exports = withPlugins([withTranspiledModules, withPreact], {
     NEXT_CMS_DOMAIN: process.env.NEXT_CMS_DOMAIN,
     WP_GRAPHQL_API: process.env.NEXT_WP_GRAPHQL_API,
     MAPBOX_ACCESS_TOKEN: process.env.MAPBOX_ACCESS_TOKEN
+  },
+
+  future: {
+    webpack5: true
   }
 });
