3ee392ca440a61391be021e13f86277d8571d205
Gustav Pursche <pursche@posteo.de>
2021-04-07 15:44:57 +0200

    Actions: Delay loading of the map until it's visible

--- components/Blocks/Actions/Actions.js
+++ components/Blocks/Actions/Actions.js
@@ -1,5 +1,6 @@
 import { useTranslation } from 'next-i18next';
 import { format } from 'date-fns';
+import { useInView } from 'react-intersection-observer';
 import dynamic from 'next/dynamic';
 
 import Action from '@/components/Teaser/Action';
@@ -9,14 +10,28 @@ const MapboxMap = dynamic(() => import('@/components/MapboxMap'));
 
 export default function ActionsBlock({ show_map = false, cta, actions = [] }) {
   const { t } = useTranslation();
+  const { ref, inView } = useInView({
+    triggerOnce: true
+  });
 
   if (actions.length === 0) {
     return null;
   }
 
   return (
-    <section className="col-span-full md:col-start-2 md:col-span-12 pt-10 md:pt-12 pb-20 md:pb-32">
-      {show_map && <MapboxMap className="mb-8 h-96" />}
+    <section
+      className="col-span-full md:col-start-2 md:col-span-12 pt-10 md:pt-12 pb-20 md:pb-32"
+      ref={ref}
+    >
+      {show_map && (
+        <>
+          {inView ? (
+            <MapboxMap className="mb-8 h-96" />
+          ) : (
+            <div className="bg-gray-500 h-96" />
+          )}
+        </>
+      )}
 
       <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
         {actions.map(({ location, start, intro, title, slug }, index) => (
