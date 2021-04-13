1b832181eca94615e932a80f985731337556eca5
Gustav Pursche <pursche@posteo.de>
2021-04-13 21:20:46 +0200

    SafeHarbour: add since label in front of date

--- components/Teaser/SafeHarbour/SafeHarbour.js
+++ components/Teaser/SafeHarbour/SafeHarbour.js
@@ -1,12 +1,15 @@
+import { useTranslation } from 'next-i18next';
 import NextLink from 'next/link';
 
 export default function SafeHarbourTeaser({ uri, name, since, description }) {
+  const { t } = useTranslation();
+
   return (
     <NextLink href={uri}>
       <a className="flex flex-col bg-orange-200 hover:bg-orange-800 p-6 md:p-10 h-full">
         <h3>
           <span className="font-rubik text-small md:text-xs italic">
-            {since || ''}
+            {since ? `${t('safeHarbour.since')} ${since}` : ''}
           </span>
           <span className="font-brezel leading-tight block text-large md:text-medium font-bold italic mt-1 mb-2 md:mb-4">
             {name}
