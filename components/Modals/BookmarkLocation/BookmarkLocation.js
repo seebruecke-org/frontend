807958c69d6f9065f332b09a8d31a1a24c3075dc
Gustav Pursche <pursche@posteo.de>
2021-04-28 22:06:28 +0200

    Modals: translate strings

--- components/Modals/BookmarkLocation/BookmarkLocation.js
+++ components/Modals/BookmarkLocation/BookmarkLocation.js
@@ -1,17 +1,24 @@
+import { useTranslation } from 'next-i18next';
+
 import Form, { TextInput } from '@/components/Form';
 import Heading from '@/components/Heading';
 import Modal from '@/components/Modal';
 
-export default function BookmarkLocationFormModal({ onClose = () => {}}) {
+export default function BookmarkLocationFormModal({ onClose = () => {} }) {
+  const { t } = useTranslation();
+
   return (
     <Modal isOpen={true} onClose={onClose}>
       <Heading level={2} as={4}>
-        Save a location
+        {t('modal.bookmark.title')}
       </Heading>
 
       <Form>
         <div className="col-span-full">
-          <TextInput placeholder="Search for a location" name="location" />
+          <TextInput
+            placeholder={t('modal.bookmark.placeholder')}
+            name="location"
+          />
         </div>
       </Form>
     </Modal>
