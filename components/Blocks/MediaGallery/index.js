9c60da6ea20e6388598b5bcd63109ef5603b4902
Gustav Pursche <pursche@posteo.de>
2021-04-28 19:53:10 +0200

    MediaGallery: scale down images proportionally

--- components/Blocks/MediaGallery/index.js
+++ components/Blocks/MediaGallery/index.js
@@ -1,7 +1,22 @@
+import { scaleImageTo } from '@/lib/media';
 import MediaGallery from './MediaGallery';
 import { FRAGMENT as FRAGMENT_MEDIA } from '@/components/Media';
 
 export default MediaGallery;
+
+async function sideloadData({ items }) {
+  return items.map(({ media: { width, height, ...media } }) => {
+    const [scaledWidth, scaledHeight] = scaleImageTo(600, [width, height]);
+
+    return {
+      media: {
+        ...media,
+        width: scaledWidth,
+        height: scaledHeight
+      }
+    };
+  });
+}
 
 export const FRAGMENT = `
   ... on ComponentSharedBlocksMediaGallery {
@@ -14,5 +29,6 @@ export const FRAGMENT = `
 export const block = {
   name: 'MediaGallery',
   Component: MediaGallery,
-  Fragment: FRAGMENT
+  Fragment: FRAGMENT,
+  sideload: sideloadData
 };
