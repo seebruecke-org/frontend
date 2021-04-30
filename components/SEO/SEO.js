239d3eb12cc071cd3d4a42871f2e645bde84c7db
Gustav Pursche <pursche@posteo.de>
2021-04-30 11:42:35 +0200

    SEO: fix VERCEL_URL env variables

--- components/SEO/SEO.js
+++ components/SEO/SEO.js
@@ -5,11 +5,11 @@ export default function SEO({ title, metadata: origMetadata = {} }) {
 
   if (Object.keys(metadata).length === 0) {
     metadata['twitter_image'] = {
-      url: process.env.VERCEL_URL + '/public/twitter-preview.png'
+      url: process.env.NEXT_PUBLIC_VERCEL_URL + '/public/twitter-preview.png'
     };
 
     metadata['facebook_image'] = {
-      url: process.env.VERCEL_URL + '/public/facebook-preview.png'
+      url: process.env.NEXT_PUBLIC_VERCEL_URL + '/public/facebook-preview.png'
     };
   }
 
@@ -36,10 +36,9 @@ export default function SEO({ title, metadata: origMetadata = {} }) {
 
             if (normalizedKey === 'image') {
               if (value?.url) {
-                value = process.env.NEXT_PUBLIC_CMS_DOMAIN + value.url;
+                value = `${process.env.NEXT_PUBLIC_CMS_DOMAIN}/${value.url}`;
               } else {
-                const baseURL = process.env.VERCEL_URL;
-                value = baseURL + `/api/screenshot?url=${baseURL}`;
+                value = `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/screenshot?url=${process.env.NEXT_PUBLIC_VERCEL_URL}`;
               }
             }
 
