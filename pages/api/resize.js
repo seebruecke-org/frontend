7f81dbb81900c3747b8f8b3800aba396b7da9bdc
Gustav Pursche <pursche@posteo.de>
2021-05-05 19:08:20 +0200

    API: make resize endpoint dynamic

--- pages/api/resize.js
+++ pages/api/resize.js
@@ -1,17 +1,66 @@
+import { serializeError } from 'serialize-error';
 import sharp from 'sharp';
+
+import { isValidSeebrueckeUrl } from '@/lib/url';
+
+const SIZES = {
+  twitter: [1200, 600],
+  facebook: [1200, 628]
+};
+
+function getSize(name) {
+  return SIZES[name];
+}
+
+function isValidSize(name) {
+  return !!SIZES[name];
+}
+
+async function resizeImage(url, size) {
+  const imageBuffer = await fetch(url).then((res) => res.arrayBuffer());
+
+  return await sharp(Buffer.from(imageBuffer))
+    .resize(...getSize(size))
+    .png()
+    .toBuffer();
+}
 
 export default async function handler(req, res) {
   const { query } = req;
-  const URL =
-    'https://seeb.uber.space/uploads/Bildschirmfoto_2021_05_04_um_12_47_10_ccb4580224.png';
 
-  const imageBuffer = await fetch(URL).then((res) => res.arrayBuffer());
+  if (query.url && isValidSeebrueckeUrl(query.url)) {
+    const { url, size } = query;
 
-  const image = await sharp(Buffer.from(imageBuffer))
-    .resize(1200, 600)
-    .png()
-    .toBuffer();
+    if (size && isValidSize(size)) {
+      const image = await resizeImage(url, size);
 
-  res.setHeader('Content-Type', 'image/png');
-  res.status(200).send(image);
+      res.setHeader('Cache-Control', `public, max-age=${60 * 60 * 24}`);
+      res.setHeader('Content-Type', 'image/png');
+      res.status(200).send(image);
+    } else {
+      res
+        .status(500)
+        .send(
+          process.env.NODE_ENV !== 'production'
+            ? JSON.stringify(
+                serializeError(
+                  new Error('You must provide a valid size param.')
+                )
+              )
+            : ''
+        );
+    }
+  } else {
+    res
+      .status(500)
+      .send(
+        process.env.NODE_ENV !== 'production'
+          ? JSON.stringify(
+              serializeError(
+                new Error('You must provide a valid Seebrücke hostname.')
+              )
+            )
+          : ''
+      );
+  }
 }
