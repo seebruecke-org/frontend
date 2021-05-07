66a9be039943efc9fd6cd5a758dbc31ec5ba2f3c
Gustav Pursche <pursche@posteo.de>
2021-05-07 12:31:34 +0200

    CTA/ Button: unify paddings/ margins across screens

--- components/CTA/CTA.js
+++ components/CTA/CTA.js
@@ -9,7 +9,7 @@ export default function CTA({ link, inverse = false }) {
     <StrapiLink
       link={link}
       className={clsx(
-        'font-rubik font-bold text-small md:text-base uppercase py-5 md:py-6 px-8 md:px-10 rounded-full inline-block text-center',
+        'font-rubik font-bold text-small md:text-base uppercase py-4 sm:py-5 md:py-6 px-7 sm:px-8 md:px-10 rounded-full inline-block text-center',
         inverse
           ? 'bg-black text-white hover:bg-white hover:text-black'
           : 'bg-white text-black hover:bg-black hover:text-white',
--- components/Form/Button/Button.js
+++ components/Form/Button/Button.js
@@ -10,7 +10,7 @@ export default function Button({
     <button
       type={type}
       className={clsx(
-        'bg-black hover:bg-white text-white hover:text-black border-black border text-xs md:text-base font-rubik font-bold uppercase rounded-full py-6 px-10 self-start',
+        'bg-black hover:bg-white text-white hover:text-black border-black border text-xs md:text-base font-rubik font-bold uppercase rounded-full py-4 sm:py-5 md:py-6 px-7 sm:px-8 md:px-10 self-start',
         className
       )}
       {...props}
