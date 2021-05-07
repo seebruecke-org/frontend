2953065232ca0d2056fc58ca0f275cee621ac68d
Gustav Pursche <pursche@posteo.de>
2021-05-07 20:19:28 +0200

    Header: translate some strings

--- components/Header/More/More.js
+++ components/Header/More/More.js
@@ -1,3 +1,4 @@
+import { useTranslation } from 'next-i18next';
 import clsx from 'clsx';
 
 import TimesIcon from '@/public/icons/times.svg';
@@ -5,26 +6,30 @@ import TimesIcon from '@/public/icons/times.svg';
 import * as styles from './more.module.css';
 
 function Footer() {
+  const { t } = useTranslation();
+
   return (
     <footer className="flex justify-end bg-gray-700 md:hidden mt-auto border-gray-600 border-t">
       <a
         href=""
         className="w-1/2 py-5 text-center font-rubik text-xs font-bold uppercase border-r border-gray-600"
       >
-        Mein Ort
+        {t('header.myPlace')}
       </a>
 
       <a
-        href=""
+        href={t('slugs.search')}
         className="w-1/2 py-5 text-center font-rubik text-xs font-bold uppercase"
       >
-        Suche
+        {t('header.search')}
       </a>
     </footer>
   );
 }
 
 export default function More({ children, onDismiss = () => {} }) {
+  const { t } = useTranslation();
+
   return (
     <div className="flex flex-col bg-gray-800 absolute top-0 md:top-full right-0 w-screen md:w-auto h-screen md:h-auto pt-5 md:pt-16 md:pl-16 md:pr-16 z-40">
       <span
@@ -41,7 +46,7 @@ export default function More({ children, onDismiss = () => {} }) {
       >
         <TimesIcon className="w-16 h-auto" />
         <span className="font-rubik text-3xs uppercase tracking-wide -mt-1">
-          Close
+          {t('modal.close')}
         </span>
       </button>
 
