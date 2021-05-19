4bf143a98a5b165824fb676a62df7945472c9ee8
Gustav Pursche <pursche@posteo.de>
2021-05-19 21:15:50 +0200

    TakePart: fix vertical paddings of all groups

--- pages/take-part/all-groups.js
+++ pages/take-part/all-groups.js
@@ -6,11 +6,13 @@ import dynamic from 'next/dynamic';
 import { getPage } from '@/lib/pages';
 import { fetchAllGroups } from '@/lib/take-part';
 import { query as queryGlobalData } from '@/lib/global';
+import { getFirstBlockName, getLastBlockName } from '@/lib/blocks';
 import useCityFilter from '@/lib/hooks/useCityFilter';
 
 import Group from '@/components/Teaser/Group';
 import BlockSwitch from '@/components/BlockSwitch';
 import Form from '@/components/Form';
+import PageBody from '@/components/PageBody';
 import Row from '@/components/Form/Row';
 import SEO from '@/components/SEO';
 import TextInput from '@/components/Form/TextInput';
@@ -89,7 +91,10 @@ export default function TakePartOverview({ cities: defaultCities, page }) {
   const citiesListRef = useRef(null);
 
   return (
-    <article>
+    <PageBody
+      firstBlock={getFirstBlockName(page?.metadata)}
+      lastBlock={getLastBlockName(page?.content)}
+    >
       <SEO title={page?.title} metadata={page?.metadata} />
 
       <BlockSwitch blocks={page?.content} />
@@ -170,7 +175,7 @@ export default function TakePartOverview({ cities: defaultCities, page }) {
           </ul>
         </div>
       </div>
-    </article>
+    </PageBody>
   );
 }
 
