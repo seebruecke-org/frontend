2e53059beacb118fbc1923fa3e4af8bc5e07ad6c
Gustav Pursche <pursche@posteo.de>
2021-05-19 21:20:15 +0200

    NewsEntry: reduce size of clickable area

--- components/Teaser/NewsEntry/NewsEntry.js
+++ components/Teaser/NewsEntry/NewsEntry.js
@@ -19,51 +19,55 @@ export default function NewsEntry({
   const translatedType = t(`news.type.${type}`);
 
   return (
-    <article className="grid grid-layout-primary gap-8 col-span-full pt-12 md:pt-20 px-8 md:px-0 relative group overflow-x-hidden">
+    <article className="grid grid-layout-primary col-span-full pt-12 md:pt-20 px-8 md:px-0 relative group overflow-x-hidden">
       <span className="col-start-1 md:col-start-2 col-span-full md:col-span-12 h-1 border-b border-gray-300 mb-4 md:mb-12" />
 
-      {image && (
-        <div className="col-span-full md:col-start-2 md:col-span-4">
-          <Media image={image} priority={index < 2} />
-        </div>
-      )}
-
-      <div
-        className={clsx(
-          'col-span-full w-full',
-          image
-            ? 'md:col-start-6 md:col-span-8'
-            : 'md:col-start-3 md:col-span-11'
+      <div className="grid grid-layout-primary gap-8 col-span-full relative">
+        {image && (
+          <div className="col-span-full md:col-start-2 md:col-span-4">
+            <Media image={image} priority={index < 2} />
+          </div>
         )}
-      >
-        <header className="font-rubik text-xs md:text-base text-gray-600 mb-1 uppercase flex">
-          {publication_date}
-          <span className="px-3">·</span>
-          {translatedType}
-        </header>
+
+        <div
+          className={clsx(
+            'col-span-full w-full',
+            image
+              ? 'md:col-start-6 md:col-span-8'
+              : 'md:col-start-3 md:col-span-11'
+          )}
+        >
+          <header className="font-rubik text-xs md:text-base text-gray-600 mb-1 uppercase flex">
+            {publication_date}
+            <span className="px-3">·</span>
+            {translatedType}
+          </header>
+
+          <NextLink href={`/${t('news.slug')}/${slug}/`}>
+            <a className="group-hover:underline">
+              <Heading level={2} as={3}>
+                {title}
+              </Heading>
+            </a>
+          </NextLink>
+
+          {excerpt && (
+            <p className="font-rubik text-small md:text-medium mt-6">
+              {excerpt}
+            </p>
+          )}
+        </div>
 
         <NextLink href={`/${t('news.slug')}/${slug}/`}>
-          <a className="group-hover:underline">
-            <Heading level={2} as={3}>
-              {title}
-            </Heading>
+          <a
+            className="absolute top-0 left-0 h-full w-full z-10 opacity-0 col-start-2 col-span-12"
+            tabIndex="-1"
+            aria-hidden
+          >
+            <Heading level={3}>{title}</Heading>
           </a>
         </NextLink>
-
-        {excerpt && (
-          <p className="font-rubik text-small md:text-medium mt-6">{excerpt}</p>
-        )}
       </div>
-
-      <NextLink href={`/${t('news.slug')}/${slug}/`}>
-        <a
-          className="absolute top-0 left-0 h-full w-full z-10 opacity-0 col-start-2 col-span-12"
-          tabIndex="-1"
-          aria-hidden
-        >
-          <Heading level={3}>{title}</Heading>
-        </a>
-      </NextLink>
     </article>
   );
 }
