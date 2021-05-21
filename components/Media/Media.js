3379ad76fed401d128267474acbf2c3850e2fcd2
Gustav Pursche <pursche@posteo.de>
2021-05-21 19:27:24 +0200

    Media: fix caption position when resizing

--- components/Media/Media.js
+++ components/Media/Media.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Portal } from 'react-portal';
 import clsx from 'clsx';
@@ -43,28 +43,36 @@ export default function Media({
   const showCaptionAsOverlay = !showCaption;
   const [isCaptionOpen, setIsCaptionOpen] = useState(showCaption);
   const [captionPosition, setCaptionPosition] = useState(null);
+  const captionToggleRef = useRef(null);
   const imageCaption = caption || mediaCaption;
   const hasCaption = !!imageCaption;
 
   return (
-    <figure className={clsx('leading-none relative text-none', className)}>
-      <div className={classNameImage}>{<NextImage {...imageProps} />}</div>
-
-      {!showCaption && hasCaption && (
-        <Measure
-          bounds
-          onResize={({ bounds }) => {
-            setCaptionPosition(bounds);
-          }}
+    <Measure
+      onResize={() => {
+        if (captionToggleRef?.current) {
+          setCaptionPosition(captionToggleRef.current.getBoundingClientRect());
+        }
+      }}
+    >
+      {({ measureRef }) => (
+        <figure
+          className={clsx('leading-none relative text-none', className)}
+          ref={measureRef}
         >
-          {({ measureRef }) => (
+          <div className={classNameImage}>{<NextImage {...imageProps} />}</div>
+
+          {!showCaption && hasCaption && (
             <button
               type="button"
-              ref={measureRef}
               onClick={() => {
                 setIsCaptionOpen(!isCaptionOpen);
               }}
-              className={clsx('absolute z-30 p-3 sm:p-1', captionIconClassName)}
+              ref={captionToggleRef}
+              className={clsx(
+                'absolute z-30 p-3 sm:p-1 outline-none',
+                captionIconClassName
+              )}
               aria-label={t(
                 isCaptionOpen ? 'media.caption.close' : 'media.caption.open'
               )}
@@ -78,40 +86,40 @@ export default function Media({
               />
             </button>
           )}
-        </Measure>
-      )}
 
-      {!showCaption && isCaptionOpen && hasCaption && (
-        <Portal>
-          <figcaption
-            className={clsx(
-              'font-rubik italic text-2xs px-10 py-5 md:p-5 md:pr-0 text-gray-100 bg-black font-rubik-features',
-              classNameCaption,
-              showCaptionAsOverlay &&
-                isCaptionOpen &&
-                'absolute z-30 px-4 md:pr-5',
-              showCaptionAsOverlay && styles.captionOverlay
-            )}
-            style={{
-              left: captionPosition?.left + captionPosition?.width + 10,
-              top: captionPosition?.top - captionPosition?.height / 2
-            }}
-          >
-            <Richtext content={imageCaption} size="tiny" />
-          </figcaption>
-        </Portal>
-      )}
-
-      {showCaption && (
-        <figcaption
-          className={clsx(
-            'font-rubik italic text-2xs px-0 md:px-10 py-5 md:p-5 md:pr-0 text-gray-600 font-rubik-features',
-            classNameCaption
+          {!showCaption && isCaptionOpen && hasCaption && (
+            <Portal>
+              <figcaption
+                className={clsx(
+                  'font-rubik italic text-2xs px-10 py-5 md:p-5 md:pr-0 text-gray-100 bg-black font-rubik-features',
+                  classNameCaption,
+                  showCaptionAsOverlay &&
+                    isCaptionOpen &&
+                    'absolute z-30 px-4 md:pr-5',
+                  showCaptionAsOverlay && styles.captionOverlay
+                )}
+                style={{
+                  left: captionPosition?.left + captionPosition?.width + 10,
+                  top: captionPosition?.top - captionPosition?.height / 2
+                }}
+              >
+                <Richtext content={imageCaption} size="tiny" />
+              </figcaption>
+            </Portal>
           )}
-        >
-          <Richtext content={imageCaption} size="tiny" />
-        </figcaption>
+
+          {showCaption && (
+            <figcaption
+              className={clsx(
+                'font-rubik italic text-2xs px-0 md:px-10 py-5 md:p-5 md:pr-0 text-gray-600 font-rubik-features',
+                classNameCaption
+              )}
+            >
+              <Richtext content={imageCaption} size="tiny" />
+            </figcaption>
+          )}
+        </figure>
       )}
-    </figure>
+    </Measure>
   );
 }
