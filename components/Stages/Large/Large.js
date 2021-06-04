ff78b8b7a944862143b33d4aaca446473ec66a6a
Gustav Pursche <pursche@posteo.de>
2021-06-04 09:24:36 +0200

    StageLarge: improve styles on small screens

--- components/Stages/Large/Large.js
+++ components/Stages/Large/Large.js
@@ -24,19 +24,24 @@ export default function StageLarge({
     subnavigation && Array.isArray(subnavigation) && subnavigation.length > 0;
 
   return (
-    <div className={clsx('relative mb-40 md:mb-52', styles.stage, className)}>
+    <div
+      className={clsx('relative md:mb-52 max-w-full', styles.stage, className)}
+    >
       <Media
         image={image}
         objectFit="cover"
         layout="fill"
         priority
-        className="col-span-full h-full w-full relative md:absolute"
+        className={clsx(
+          'col-span-full w-full relative md:absolute',
+          styles.image
+        )}
         captionIconClassName="top-4 md:bottom-4 md:top-auto left-2 md:left-4"
       />
 
       <div
         className={clsx(
-          'bg-orange-200 md:w-8/12 absolute -bottom-32 md:-bottom-32 left-2 md:left-auto right-2 md:right-0 w-auto',
+          'bg-orange-200 md:w-8/12 ml-2 md:ml-0 mr-2 md:mr-0 -mt-64 md:absolute md:-bottom-32 md:left-auto md:right-0 w-auto relative z-10',
           hasSubnavigation ? 'pt-20 md:pt-28' : 'py-20 md:py-28'
         )}
       >
@@ -73,7 +78,8 @@ export default function StageLarge({
           <SectionNavigation
             items={subnavigation}
             primaryGrid={false}
-            className="px-8 md:px-20 mt-10 md:mt-20"
+            className="md:px-20 mt-10 md:mt-20"
+            alwaysShowBorder
           />
         )}
       </div>
