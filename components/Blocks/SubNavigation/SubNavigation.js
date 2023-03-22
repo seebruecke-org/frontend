78e35e0f79088726d0c35882210434007368e480
Azin <aazadi@gmail.com>
2023-03-22 18:00:24 +0100

    Fix: Navigation doesn't show on some pages

--- components/Blocks/SubNavigation/SubNavigation.js
+++ components/Blocks/SubNavigation/SubNavigation.js
@@ -3,7 +3,7 @@ import SectionNavigation from '@/components/SectionNavigation';
 export default function SubNavigationBlock(props) {
   return (
     <div className="col-span-full sticky top-0 z-30">
-      <SectionNavigation {...props} />
+      <SectionNavigation {...props} items={props['csbsn_items']} />
     </div>
   );
 }
--- components/SectionNavigation/SectionNavigation.js
+++ components/SectionNavigation/SectionNavigation.js
@@ -1,47 +1,50 @@
-import { useRouter } from 'next/router';
-import { useState, useEffect, forwardRef, useRef } from 'react';
-import clsx from 'clsx';
-import dynamic from 'next/dynamic';
+import { useRouter } from "next/router";
+import { useState, useEffect, forwardRef, useRef } from "react";
+import clsx from "clsx";
+import dynamic from "next/dynamic";
 
-import useWindowWidth from '@/lib/hooks/useWindowWidth';
-import ChevronLeftIcon from '@/public/icons/chevron-left-regular.svg';
-import ChevronRightIcon from '@/public/icons/chevron-right-regular.svg';
-import StrapiLink from '@/components/StrapiLink';
+import useWindowWidth from "@/lib/hooks/useWindowWidth";
+import ChevronLeftIcon from "@/public/icons/chevron-left-regular.svg";
+import ChevronRightIcon from "@/public/icons/chevron-right-regular.svg";
+import StrapiLink from "@/components/StrapiLink";
 
-import { arePathsEqual } from '@/lib/slug';
+import { arePathsEqual } from "@/lib/slug";
 
-import * as styles from './sectionNavigation.module.css';
+import * as styles from "./sectionNavigation.module.css";
 
-const SwiperContainer = dynamic(() => import('./Swiper'), {
+const SwiperContainer = dynamic(() => import("./Swiper"), {
   ssr: false
 });
 
 function Control({ direction, ...props }) {
-  const isDirectionPrev = direction === 'prev';
+  const isDirectionPrev = direction === "prev";
   const Icon = isDirectionPrev ? ChevronLeftIcon : ChevronRightIcon;
 
   return (
     <button
       type="button"
       className={clsx(
-        'absolute -top-1 h-full z-10 flex items-center',
+        "absolute -top-1 h-full z-10 flex items-center",
         isDirectionPrev
-          ? 'left-0 md:-left-4 pl-4 pr-2'
-          : 'right-0 md:-right-4 pr-4 pl-2',
+          ? "left-0 md:-left-4 pl-4 pr-2"
+          : "right-0 md:-right-4 pr-4 pl-2",
         isDirectionPrev ? styles.fadeOutStart : styles.fadeOutEnd
       )}
       {...props}
     >
       <Icon
         className={clsx(
-          'w-4 h-auto flex-shrink-0 mix-blend-multiply opacity-40'
+          "w-4 h-auto flex-shrink-0 mix-blend-multiply opacity-40"
         )}
       />
     </button>
   );
 }
 
-function Wrapped({ swiper = false, items, setItemRef = () => {} }) {
+function Wrapped({
+                   swiper = false, items, setItemRef = () => {
+  }
+                 }) {
   const { asPath } = useRouter();
   const [allowNext, setAllowNext] = useState(false);
   const [allowPrev, setAllowPrev] = useState(false);
@@ -92,9 +95,9 @@ function Wrapped({ swiper = false, items, setItemRef = () => {} }) {
               <StrapiLink
                 link={item}
                 className={clsx(
-                  'block py-3 px-2 md:p-3 uppercase font-rubik font-rubik-features font-bold text-xs md:text-base leading-none whitespace-nowrap col-span-2 text-center',
-                  isActive ? 'bg-white' : 'hover:bg-white',
-                  index === 0 && 'col-start-2'
+                  "block py-3 px-2 md:p-3 uppercase font-rubik font-rubik-features font-bold text-xs md:text-base leading-none whitespace-nowrap col-span-2 text-center",
+                  isActive ? "bg-white" : "hover:bg-white",
+                  index === 0 && "col-start-2"
                 )}
               />
             );
@@ -124,9 +127,9 @@ function Wrapped({ swiper = false, items, setItemRef = () => {} }) {
           <StrapiLink
             link={item}
             className={clsx(
-              'block py-3 px-2 md:p-3 uppercase font-rubik font-rubik-features font-bold text-xs md:text-base leading-none whitespace-nowrap col-span-2 text-center',
-              isActive ? 'bg-white' : 'hover:bg-white',
-              index === 0 && 'col-start-2'
+              "block py-3 px-2 md:p-3 uppercase font-rubik font-rubik-features font-bold text-xs md:text-base leading-none whitespace-nowrap col-span-2 text-center",
+              isActive ? "bg-white" : "hover:bg-white",
+              index === 0 && "col-start-2"
             )}
             ref={(ref) => setItemRef(ref)}
           />
@@ -137,10 +140,9 @@ function Wrapped({ swiper = false, items, setItemRef = () => {} }) {
 }
 
 export default forwardRef(function SectionNavigation(
-  { csbsn_items = [], className, primaryGrid = true, alwaysShowBorder = false },
+  { items = [], className, primaryGrid = true, alwaysShowBorder = false },
   ref
 ) {
-  let items = csbsn_items;
   const containerRef = useRef(null);
   const itemRefs = [];
   const [useSwiper, setUseSwiper] = useState(false);
@@ -163,25 +165,25 @@ export default forwardRef(function SectionNavigation(
     <div
       ref={ref}
       className={clsx(
-        'bg-orange-200',
-        primaryGrid && 'grid grid-layout-primary',
-        'py-6 sm:py-7 md:py-8 relative -top-px',
+        "bg-orange-200",
+        primaryGrid && "grid grid-layout-primary",
+        "py-6 sm:py-7 md:py-8 relative -top-px",
         styles.navigation,
         className
       )}
     >
       <span
         className={clsx(
-          'w-full bg-gray-400 absolute top-0 left-0 opacity-50 mix-blend-multiply',
-          alwaysShowBorder ? 'block' : 'hidden md:block',
+          "w-full bg-gray-400 absolute top-0 left-0 opacity-50 mix-blend-multiply",
+          alwaysShowBorder ? "block" : "hidden md:block",
           styles.border
         )}
       />
 
       <nav
         className={clsx(
-          primaryGrid && 'col-span-full md:col-start-3 md:col-span-10',
-          'relative w-full px-8 md:px-0'
+          primaryGrid && "col-span-full md:col-start-3 md:col-span-10",
+          "relative w-full px-8 md:px-0"
         )}
         ref={containerRef}
       >
