3c8d4a0d64e0ff0df37552fed0cbe138fa06946a
Azin <aazadi@gmail.com>
2023-04-19 09:30:54 +0200

    no article image for commentaries

--- pages/news/[...slug].js
+++ pages/news/[...slug].js
@@ -1,69 +1,59 @@
-import { useTranslation } from 'next-i18next';
-import hirestime from 'hirestime';
+import { useTranslation } from "next-i18next";
+import hirestime from "hirestime";
 
-import { createClient } from '@/lib/api';
-import { getLastBlockName } from '@/lib/blocks';
-import { query as queryGlobalData } from '@/lib/global';
-import { fetchNewsBySlug, fetchAllNewsPaths } from '@/lib/news';
-import logger from '@/lib/logger';
+import { createClient } from "@/lib/api";
+import { getLastBlockName } from "@/lib/blocks";
+import { query as queryGlobalData } from "@/lib/global";
+import { fetchNewsBySlug, fetchAllNewsPaths } from "@/lib/news";
+import logger from "@/lib/logger";
 
-import { Image as RichtextBlockImage } from '@/components/Richtext';
-import BlockSwitch from '@/components/BlockSwitch';
-import Heading from '@/components/Blocks/Heading';
-import PageBody from '@/components/PageBody';
-import SEO from '@/components/SEO';
+import { Image as RichtextBlockImage } from "@/components/Richtext";
+import BlockSwitch from "@/components/BlockSwitch";
+import Heading from "@/components/Blocks/Heading";
+import PageBody from "@/components/PageBody";
+import SEO from "@/components/SEO";
 
 export default function NewsEntryPage({
-  title,
-  metadata,
-  content,
-  image,
-  publishedAt,
-  type
-}) {
-  const { t } = useTranslation('news');
+                                        title, metadata, content, image, publishedAt, type
+                                      }) {
+  const { t } = useTranslation("news");
+  let bp = {};
+  if (type !== "commentary") {
+    bp = {
+      Richtext: {
+        renderers: {
+          image: ({ src, alt }) => {
+            return (<RichtextBlockImage
+                image={{
+                  caption: image?.caption || image?.media?.caption, media: {
+                    url: src, width: 270, height: 270, alternativeText: alt
+                  }
+                }}
+                priority
+                className="md:float-left md:mr-12 mb-10 md:-ml-16 xl:-ml-48 md:mt-4"
+              />);
+          }
+        }
+      }
+    };
+  }
 
-  return (
-    <>
-      <SEO title={title} metadata={metadata} />
+  return (<>
+    <SEO title={title} metadata={metadata} />
 
-      <PageBody lastBlock={getLastBlockName(content)}>
-        <div className="grid grid-layout-primary">
-          <Heading level={1} kicker={`${publishedAt} · ${t(`type.${type}`)}`}>
-            {title}
-          </Heading>
-        </div>
+    <PageBody lastBlock={getLastBlockName(content)}>
+      <div className="grid grid-layout-primary">
+        <Heading level={1} kicker={`${publishedAt} · ${t(`type.${type}`)}`}>
+          {title}
+        </Heading>
+      </div>
 
-        <BlockSwitch
-          blocks={content}
-          blockProps={{
-            Richtext: {
-              renderers: {
-                // eslint-disable-next-line react/display-name
-                image: ({ src, alt }) => {
-                  return (
-                    <RichtextBlockImage
-                      image={{
-                        caption: image?.caption || image?.media?.caption,
-                        media: {
-                          url: src,
-                          width: 270,
-                          height: 270,
-                          alternativeText: alt
-                        }
-                      }}
-                      priority
-                      className="md:float-left md:mr-12 mb-10 md:-ml-16 xl:-ml-48 md:mt-4"
-                    />
-                  );
-                }
-              }
-            }
-          }}
-        />
-      </PageBody>
-    </>
-  );
+      <BlockSwitch
+        blocks={content}
+        blockProps={bp}
+      />
+    </PageBody>
+  </>);
 }
 
 export async function getStaticPaths({ defaultLocale }) {
@@ -71,48 +61,34 @@ export async function getStaticPaths({ defaultLocale }) {
   const paths = await fetchAllNewsPaths(defaultLocale, { client });
 
   return {
-    fallback: true,
-    paths
+    fallback: true, paths
   };
 }
 
 export async function getStaticProps({ locale, params: { slug } }) {
   const getElapsed = hirestime();
   const client = createClient();
-  const { initialState = null, ...globalData } = await queryGlobalData(
-    locale,
-    ['news'],
-    { client }
-  );
+  const { initialState = null, ...globalData } = await queryGlobalData(locale, ["news"], { client });
   const { format } = globalData._nextI18Next.initialI18nStore[locale];
   const { data } = await fetchNewsBySlug(slug, locale, format, { client });
 
   if (data === null) {
     logger.error({
-      message: '404',
-      locale,
-      path: `${locale}/news/[${slug}]`
+      message: "404", locale, path: `${locale}/news/[${slug}]`
     });
 
     return {
-      notFound: true,
-      revalidate: 10
+      notFound: true, revalidate: 10
     };
   }
 
   logger.info({
-    message: 'timing',
-    locale,
-    path: `${locale}/news/[${slug}]`,
-    time: getElapsed.seconds()
+    message: "timing", locale, path: `${locale}/news/[${slug}]`, time: getElapsed.seconds()
   });
 
   return {
-    revalidate: 60 * 5,
-    props: {
-      ...data,
-      ...globalData,
-      initialState
+    revalidate: 60 * 5, props: {
+      ...data, ...globalData, initialState
     }
   };
 }
