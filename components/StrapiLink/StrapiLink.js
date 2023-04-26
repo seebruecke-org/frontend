964b65f79872eff437ede871223b839c0de5cae0
Azin <aazadi@gmail.com>
2023-04-26 10:05:33 +0200

    Respect link's locale when it is pointing to not the current locale

--- components/StrapiLink/StrapiLink.js
+++ components/StrapiLink/StrapiLink.js
@@ -3,7 +3,9 @@ import Link from 'next/link';
 
 function StrapiLink({ link, children, locale, ...props }, ref) {
   let linkProps = link;
-
+  if (linkProps.locale !== "Context") {
+    locale = linkProps.locale;
+  }
   if (!linkProps.url) {
     return (
       <span {...props} ref={ref}>
