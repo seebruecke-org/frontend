af43de07efc9a8e519ba67918ded32d0c55c2736
Azin <aazadi@gmail.com>
2023-07-03 10:42:26 +0200

    embed: narrow instagram

--- components/embed/providers/instagram.js
+++ components/embed/providers/instagram.js
@@ -10,10 +10,12 @@ export default function Instagram(embed_data) {
   return (
     <>
       {active && (
-        <InstagramEmbed
-          url={embed_data.url}
-          placeholderDisabled
-        />
+        <div className="instagram-embed">
+          <InstagramEmbed
+            url={embed_data.url}
+            placeholderDisabled
+          />
+        </div>
       )}
       {!active && (
         <div
@@ -23,15 +25,15 @@ export default function Instagram(embed_data) {
               {t("embed.default.title", { provider: "Instagram" })}
             </h2>
             <p className="font-brezel text-base md:text-medium leading-tight
-      mt-5  mx-2 sm:mx-4 md:mx-0 z-10">
+                          mt-5  mx-2 sm:mx-4 md:mx-0 z-10">
               {t("embed.default.description")}
             </p>
             <button
               className="
-          whitespace-no-wrap text-gray-700 hover:text-white
-          hover:bg-black bg-white text-xs md:text-base
-          font-rubik font-bold uppercase rounded-full
-          py-5 sm:py-5 md:py-6 px-9 sm:px-8 md:px-10 mt-32"
+              whitespace-no-wrap text-gray-700 hover:text-white
+              hover:bg-black bg-white text-xs md:text-base
+              font-rubik font-bold uppercase rounded-full
+              py-5 sm:py-5 md:py-6 px-9 sm:px-8 md:px-10 mt-32"
               onClick={() => setActive(true)}
             >
 
