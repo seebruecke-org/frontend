d03f278d3be69b6453d54969bfa3237724774c6e
Azin <aazadi@gmail.com>
2023-11-29 11:10:11 +0100

    Fundraising box loads on menu changes

--- components/Fundraisingbox/Fundraisingbox.js
+++ components/Fundraisingbox/Fundraisingbox.js
@@ -1,16 +1,27 @@
-import { useTranslation } from 'next-i18next';
-import Script from 'react-load-script';
+import {useTranslation} from 'next-i18next';
+import {useEffect, useRef} from 'react'
+
 
 export default function Fundraisingbox({ scriptUrl }) {
-  const { t } = useTranslation();
+  const {t} = useTranslation();
+
+  const scriptRoot = useRef(); // gets assigned to a root node
+  const script = `<script type='text/javascript' src=${scriptUrl} ></script>`;
+
+  useEffect(() => {
+    // creates a document range (grouping of nodes in the document is my understanding)
+    // in this case we instantiate it as empty, on purpose
+    const range = document.createRange();
+    // creates a mini-document (light weight version), in our range with our script in it
+    const documentFragment = range.createContextualFragment(script);
+    // appends it to our script root - so it renders in the correct location
+    scriptRoot.current.append(documentFragment);
+  });
 
   return (
     <div>
       {/* This div is used by the FB to inject all it's content */}
-      <div id="fbIframeDiv" className="relative" />
-
-      <Script url={scriptUrl} />
-
+      <div id="fbIframeDiv" className="relative"/>
       <noscript>{t('fundraisingbox.activateJS')}</noscript>
 
       <a
