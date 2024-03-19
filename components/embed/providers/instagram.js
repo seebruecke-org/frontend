13c6d2948e0dffb498ee8a7d587da15530e9d480
Azin <aazadi@gmail.com>
2024-03-19 11:16:48 +0100

    embed: fix instagram embed

--- components/embed/providers/instagram.js
+++ components/embed/providers/instagram.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import { useTranslation } from "next-i18next";
-import { InstagramEmbed } from "react-social-media-embed";
+import React, { useState } from 'react';
+import { useTranslation } from 'next-i18next';
+import { InstagramEmbed } from 'react-social-media-embed';
 
 export default function Instagram(embed_data) {
   const { t } = useTranslation();
@@ -11,22 +11,20 @@ export default function Instagram(embed_data) {
     <>
       {active && (
         <div className="instagram-embed">
-          <InstagramEmbed
-            url={embed_data.url}
-            placeholderDisabled
-          />
+          <InstagramEmbed url={embed_data.url} placeholderDisabled />
         </div>
       )}
       {!active && (
-        <div
-          className="text-white bg-orange-800">
+        <div className="text-white bg-orange-800">
           <div className="px-8 md:px-16 md:py-16">
             <h2 className="block font-brezel text-xl italic font-bold leading-none mb-10">
-              {t("embed.default.title", { provider: "Instagram" })}
+              {t('embed.default.title', { provider: 'Instagram' })}
             </h2>
-            <p className="font-brezel text-base md:text-medium leading-tight
-                          mt-5  mx-2 sm:mx-4 md:mx-0 z-10">
-              {t("embed.default.description")}
+            <p
+              className="font-brezel text-base md:text-medium leading-tight
+                          mt-5  mx-2 sm:mx-4 md:mx-0 z-10"
+            >
+              {t('embed.default.description', { provider: 'Meta' })}
             </p>
             <button
               className="
@@ -36,10 +34,8 @@ export default function Instagram(embed_data) {
               py-5 sm:py-5 md:py-6 px-9 sm:px-8 md:px-10 mt-32"
               onClick={() => setActive(true)}
             >
-
-              {t("embed.default.button")}
+              {t('embed.default.button')}
             </button>
-
           </div>
         </div>
       )}
