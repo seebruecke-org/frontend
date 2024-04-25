8330650085245d9fc380f584851fd1b1ac081661
7azin <aazadi+1@gmail.com>
2024-04-25 09:13:51 +0200

    Pagination - Use navigation buttons instead of list of pages

--- components/Pagination/Pagination.js
+++ components/Pagination/Pagination.js
@@ -1,16 +1,62 @@
 import styles from './pagination.module.sass';
+import Link from "next/link";
+import Image from "next/image";
+import clsx from "clsx";
+import ForwardImage from "./Forward.svg"
+import FastForwardImage from "./FastForward.svg"
+import BackwardImage from "./Backward.svg"
+import FastBackwardImage from "./FastBackward.svg"
+import PaginationButton from "@/components/Pagination/PaginationButton";
 
-export default function Pagination({total, current, linkMaker}) {
+export default function Pagination({total, current, urlMaker, locale}) {
+
   return <div className="col-span-full pb-20 md:pb-40">
     <div className="grid grid-layout-primary">
       <div className="col-span-full md:col-start-2 md:col-span-12">
         <ul className={styles.pagination}>
-          {[...Array(total)].map((x, i) => (
-            <li>
-              {linkMaker && linkMaker(i, i == current)}
-            </li>))}
+          <li>
+            <PaginationLink pageNum={current - 10} locale={locale} urlMaker={urlMaker} total={total}>
+              <FastBackwardImage/>
+            </PaginationLink>
+          </li>
+          <li>
+            <PaginationLink pageNum={current - 1} locale={locale} urlMaker={urlMaker} total={total}>
+              <BackwardImage/>
+            </PaginationLink>
+          </li>
+          <li className='font-rubik-features font-rubik text-small md:text-medium font-bold'>
+            {current+1} / {total}
+          </li>
+          <li>
+            <PaginationLink pageNum={current + 1} locale={locale} urlMaker={urlMaker} total={total}>
+              <ForwardImage/>
+            </PaginationLink>
+          </li>
+          <li>
+            <PaginationLink pageNum={current + 10} locale={locale} urlMaker={urlMaker} total={total}>
+              <FastForwardImage/>
+            </PaginationLink>
+          </li>
+
         </ul>
       </div>
     </div>
   </div>;
+}
+
+export function PaginationLink({urlMaker, pageNum, total, children, locale}) {
+  const enabled = 0 <= pageNum && pageNum < total;
+
+  const cls = 'font-rubik-features font-rubik text-small md:text-medium font-bold';
+  return (<>
+    {enabled && (<Link href={urlMaker(pageNum, total, locale)}>
+      <a className={clsx(cls, "text-black")}>
+        {children}
+      </a>
+    </Link>)}
+    {!enabled && (<span className={clsx(cls, "text-gray-500")}>
+          {children}
+        </span>)}
+  </>);
+
 }
