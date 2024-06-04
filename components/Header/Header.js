652799b2b6e7923a755bc6500dcb84203d1a9889
7azin <aazadi+1@gmail.com>
2024-06-04 08:02:47 +0200

    locales: update locale switcher links to use static hrefs

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -178,6 +178,7 @@ export default function Header({ metaItems, items }) {
                 </a>
             ))}
 
+            <Bookmark />
 
             {metaItems &&
               metaItems.length > 0 &&
